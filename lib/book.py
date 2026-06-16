c3edd4b02129bf83299baac03b2123edd8e08b59
lincoln <lincolnmwangi585@gmail.com>
2026-06-16 13:50:35 +0300

    Create book and cofee classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+        if not isinstance(self.page_count, int):
+            print("page_count must be an integer")
+            return
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+book1 = Book(title='Age is just a number', page_count='12')
+book1.turn_page()
